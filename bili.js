1fe5814a1aa8671644b7ecd7c035231a9218a9d3
hgit2010 <h20101011@gmail.com>
2026-08-07 18:20:03 +0800

    20260807

--- bili.js
+++ bili.js
@@ -20,7 +20,7 @@
 
 
 "sites":[
-{"key":"bili","name":"B站|Bili[jar]","type":3,"api":"csp_Bili","searchable":1,"quickSearch":0,"changeable":0,"ext":{	"type":"小姐姐4K#演唱会#电影#电视剧#帕梅拉#广场舞#纪录片#综艺#鬼畜#音乐#舞蹈#影视#知识#美食#风光#田园#户外#8K#徐云",	"cookie":""}},
+{"key":"bili","name":"B站|Bili[jar]","type":3,"api":"csp_Bili","searchable":1,"quickSearch":1,"changeable":1,"ext":{	"type":"小姐姐4K#演唱会#电影#电视剧#帕梅拉#广场舞#纪录片#综艺#鬼畜#音乐#舞蹈#影视#知识#美食#风光#田园#户外#8K#徐云",	"cookie":""}},
 {"key":"我的哔哩sq","name":"我的哔哩sq","type":3,"api":"./lib/drpy2.min.sq.js","ext":"./lib/我的哔哩sq.js"}
 //{"key":"说明","name":"------公众号【云星日记】------","type":3,"api":"360","searchable":1,"quickSearch":1,"filterable":1},
 //{"key":"说明2","name":"------提供免费更新维护------","type":3,"api":"360","searchable":1,"quickSearch":1,"filterable":1},
